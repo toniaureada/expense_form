9b7d413a68a0518a6d933ca95ebfcc40ac86267f
Nob Y <nob@nnusunn.jp>
2013-10-27 14:22:56 +0900

    made better checkboxes, radio button
    ..and clean up code, some commenting.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
-(function() {
-            var divs = document.getElementById('ss-form').
-            getElementsByTagName('div');
-            var numDivs = divs.length;
-            for (var j = 0; j < numDivs; j++) {
-                if (divs[j].className == 'errorbox-bad') {
-                    divs[j].lastChild.firstChild.lastChild.focus();
-                    return;
+    /*
+     * script from the original page.
+     */
+    (function() {
+                var divs = document.getElementById('ss-form').
+                getElementsByTagName('div');
+                var numDivs = divs.length;
+                for (var j = 0; j < numDivs; j++) {
+                    if (divs[j].className == 'errorbox-bad') {
+                        divs[j].lastChild.firstChild.lastChild.focus();
+                        return;
+                    }
                 }
-            }
-            for (var i = 0; i < numDivs; i++) {
-                var div = divs[i];
-                if (div.className == 'ss-form-entry' &&
-                div.firstChild &&
-                div.firstChild.className == 'ss-q-title') {
-                    div.lastChild.focus();
-                    return;
+                for (var i = 0; i < numDivs; i++) {
+                    var div = divs[i];
+                    if (div.className == 'ss-form-entry' &&
+                    div.firstChild &&
+                    div.firstChild.className == 'ss-q-title') {
+                        div.lastChild.focus();
+                        return;
+                    }
                 }
-            }
-})();
+    })();
 
 $(function() {
+    /*
+     * form fields preparation.
+     */
+    // date field
     var date = new Date();
     var yy = date.getYear();
     var mm = date.getMonth() + 1;
@@ -28,7 +35,8 @@ $(function() {
     if (mm < 10) { mm = "0" + mm; }
     if (dd < 10) { dd = "0" + dd; }
     $("#entry_0").attr("value", mm + "/" + dd + "/" + yy).datepicker();
-    $(".ss-choices").buttonset();
+
+    //item name & store name field
     $("#entry_1, #entry_7").change(function(){
         if ($(this).children(":selected").text() === "NOT A REGULAR") {
             $(this).next().show();
@@ -36,16 +44,37 @@ $(function() {
             $(this).next().hide();
         }
     })
-    $("input[type=submit]").button();
+    //checkboxs & radio buttons.
+    $(".ss-choices").buttonset();
+    $("input[type='checkbox']").click(function() { //jQuery UI bugfix (http://goo.gl/jKc8f3)
+        $(this).next().removeClass("ui-state-focus ui-state-hover");
+    });
 
-    // initialize validator
+    //submit button.
+    $("input[type='submit']").button();
+    $("input[type='submit']").click(function() { //jQuery UI bugfix (http://goo.gl/jKc8f3)
+        $(this).removeClass("ui-state-focus ui-state-hover");
+    });
+
+    /*
+     * initialize input validator
+     */
     var v_conf = {
         position: 'top left',
-        offset: [0, -20],
-        message: '<div class="ui-state-highlight ui-corner-all"><em class="arrow"/><em class="ui-icon ui-icon-alert left"/></div>'
+        offset: [0, -50],
+        message: '<div class="ui-state-highlight ui-corner-all"><em class="arrow"/><em class="ui-icon ui-icon-alert left"/></div>',
+         onBeforeValidate: function(e, els)  {
+            //reset previous errors.
+             all_fields.data("validator").reset();
+         },
+         onFail: function (e, els) {
+            //scroll to the first error position.
+            $('html, body').animate({ scrollTop: els[0].input.offset().top - 32}, 500);
+         }
     }
+    var all_fields = $('#ss-form').validator(v_conf);
     //add custom validation funcs.
-    $.tools.validator.fn(
+    $.tools.validator.fn( //item name fields.
         "#entry_2",
         "Please complete this mandatory field.",
         function(el, v) {
@@ -55,7 +84,7 @@ $(function() {
             return true;
         }
     );
-    $.tools.validator.fn(
+    $.tools.validator.fn( //store name fields.
         "#entry_8",
         "Please complete this mandatory field.",
         function(el, v) {
@@ -65,7 +94,7 @@ $(function() {
             return true;
         }
     );
-    $.tools.validator.fn(
+    $.tools.validator.fn( //checkboxes.
         "#group_5_1",
         "Please check one at least.",
         function(el, v) {
@@ -73,8 +102,4 @@ $(function() {
             return (len === 0) ? false : true;
         }
     );
-    var all_fields = $('#ss-form').validator(v_conf);
-    $("[name='submit']").click(function(event) {
-        all_fields.data("validator").reset();
-    });
 });
